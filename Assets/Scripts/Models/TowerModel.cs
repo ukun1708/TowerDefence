5f7083a5d874190529241e766eb33c0e2cce7d78
Виталий Лукин <mr.lukin1990@mail.ru>
2020-12-15 21:39:33 +0300

    Create tower model

---
+++ Assets/Scripts/Models/TowerModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerModel : MonoBehaviour
+{
+    public int damage = 10;
+    public float speedShot = 5f;
+}
