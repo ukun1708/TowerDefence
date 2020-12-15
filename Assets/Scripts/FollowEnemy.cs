6cfa25d2f90d8a8926703710e7272a205522979c
Виталий Лукин <mr.lukin1990@mail.ru>
2020-12-15 21:05:13 +0300

    Create follow enemy

---
+++ Assets/Scripts/FollowEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowEnemy : MonoBehaviour
+{
+
+    public float speedRotation = 5f;
+
+    Vector3 direction;
+
+    void Update()
+    {
+        var target = GameObject.FindWithTag("Enemy");
+
+        var dist = Vector3.Distance(transform.position, target.transform.position);
+
+        if (dist < 50f)
+        {
+            direction = transform.position - target.transform.position;
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * speedRotation);
+        }
+        
+    }
+}
