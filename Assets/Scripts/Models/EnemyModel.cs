953c3843c0a499c2c03eed22e7da1f96c544c5b8
Виталий Лукин <mr.lukin1990@mail.ru>
2020-12-15 21:39:06 +0300

    Create Enemy model

---
+++ Assets/Scripts/Models/EnemyModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyModel : MonoBehaviour
+{
+    public int Health = 100;
+    public int Damage = 10;
+    public int GetGold = 1;
+}
