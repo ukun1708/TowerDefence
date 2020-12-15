78e549666eddd31226fcb947250c128877fde356
Виталий Лукин <mr.lukin1990@mail.ru>
2020-12-15 21:04:54 +0300

    Create Enemy Creator

---
+++ Assets/Scripts/EnemyCreator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class EnemyCreator : MonoBehaviour
+{
+    public GameObject enemyPrefabs;
+
+    public Transform pointSpawn;
+
+    //public Transform[] movementPoins;
+
+    public float timeCreate;
+
+    public float timeRepeat;
+
+    public CinemachineSmoothPath path;
+
+    void Start()
+    {
+        InvokeRepeating("Create", timeCreate, timeRepeat);
+    }
+    void Update()
+    {
+        
+    }
+
+    void Create()
+    {
+        enemyPrefabs.GetComponent<PathMover>().m_Path = path;
+        Instantiate(enemyPrefabs, pointSpawn.position, Quaternion.identity);        
+    }
+}
