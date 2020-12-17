3314cdcf8824de97b6a48bddb4e76e88c08ac4bb
Виталий Лукин <mr.lukin1990@mail.ru>
2020-12-17 21:02:52 +0300

    Create enemy wave Model

---
+++ Assets/Scripts/Models/EnemyWaveModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Wave", menuName = "ScriptableObjects/WaveModel", order = 1)]
+public class EnemyWaveModel : ScriptableObject
+{
+    public int health;
+    public int damage;
+    public int getGold;
+    public float speed;
+    public int minEnemyCount;
+    public int maxEnemyCount;
+    public float timeWait;
+}
