ec5758b412bedd4670c31529314503bd473286c8
Виталий Лукин <mr.lukin1990@mail.ru>
2020-12-21 19:34:26 +0300

    Create HealthCount.cs

---
+++ Assets/Scripts/ui/HealthCount.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class HealthCount : MonoBehaviour
+{
+    public TextMeshProUGUI healthText;
+    void Update()
+    {
+        healthText.text = PlayerModel.Singleton.health.ToString();
+    }
+}
