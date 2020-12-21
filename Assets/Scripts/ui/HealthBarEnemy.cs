bd471a694f0a7c535c0fed642d81f019524bba7a
Виталий Лукин <mr.lukin1990@mail.ru>
2020-12-21 13:44:13 +0300

    Create HealthBarEnemy.cs

---
+++ Assets/Scripts/ui/HealthBarEnemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarEnemy : MonoBehaviour
+{
+    public GameObject indicator;
+
+    void Update()
+    {
+        gameObject.transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+    }
+
+    public void HealthBarUpdate(float current)
+    {
+        indicator.transform.localScale = new Vector3(1f, 1f, current);
+    }
+}
