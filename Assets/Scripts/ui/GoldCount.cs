5321bf54bf576ef2f0677e5c8c64d515aba55856
Виталий Лукин <mr.lukin1990@mail.ru>
2020-12-21 19:34:21 +0300

    Create GoldCount.cs

---
+++ Assets/Scripts/ui/GoldCount.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GoldCount : MonoBehaviour
+{
+    public TextMeshProUGUI goldText;
+    void Update()
+    {
+        goldText.text = PlayerModel.Singleton.gold.ToString();
+    }
+}
