5a2035bf140b38b6b91e42d65fe26b9bf9b7a5a1
Виталий Лукин <mr.lukin1990@mail.ru>
2020-12-22 13:48:52 +0300

    Create RestartScene.cs

---
+++ Assets/Scripts/ui/RestartScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScene : MonoBehaviour
+{
+   public void RetryScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
