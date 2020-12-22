7cea2ec1de1745e10fcf71f1c4fafcd292571c63
Виталий Лукин <mr.lukin1990@mail.ru>
2020-12-22 13:48:46 +0300

    Create DestroyedEnemiesCount.cs

---
+++ Assets/Scripts/ui/DestroyedEnemiesCount.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DestroyedEnemiesCount : MonoBehaviour
+{
+    public int destroyedEnemyCount;
+
+    public TextMeshProUGUI destroyedEnemyText;
+
+    public TextMeshProUGUI titleText;
+
+    public static DestroyedEnemiesCount Singleton;
+    void Start()
+    {
+        Singleton = this;
+
+        destroyedEnemyCount = 0;
+    }
+    private void Update()
+    {
+        destroyedEnemyText.text = "Destroyed enemies: " + destroyedEnemyCount.ToString();
+
+        if (PlayerModel.Singleton.health < 1)
+        {
+            titleText.text = "YOU LOSE";
+        }
+    }
+}
