c0ece52c61801ac2bf03add2a36c5c7a829d9143
Виталий Лукин <mr.lukin1990@mail.ru>
2020-12-23 13:36:26 +0300

    Create BulletModel.cs

---
+++ Assets/Scripts/BulletModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletModel : MonoBehaviour
+{
+    public float speedBullet = 10f;
+
+    float damage;
+
+    public TowerModel towerModel;
+
+    public EnemyWaveModel enemyModel;
+
+    void Start()
+    {
+        damage = towerModel.damage;
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * speedBullet * Time.deltaTime, Space.Self);
+
+        Destroy(gameObject, 2f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var obj = other.transform.GetComponent<Idamage>();
+
+        if (obj != null)
+        {
+            obj.TakeDamage(damage);
+
+            Destroy(gameObject);
+        }
+    }
+}
